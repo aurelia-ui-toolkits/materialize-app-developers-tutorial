96cdbc646526c0a1574e909f9920fd541f2b5a74
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-03 20:36:17 +0100

    added bridge to config

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
+    .developmentLogging()
+    .plugin('aurelia-materialize-bridge', bridge => bridge.useAll());
 
   //Uncomment the line below to enable animation.
   //aurelia.use.plugin('aurelia-animator-css');
