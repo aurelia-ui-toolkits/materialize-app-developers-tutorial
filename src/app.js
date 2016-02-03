380611b91a0303d6a0cbe9d5af7ebfd47d5e3f01
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-03 22:24:18 +0100

    added slider

--- src/app.js
+++ src/app.js
@@ -6,7 +6,8 @@ export class App {
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
       { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
       { route: 'md-select',  name: 'md-select', moduleId: 'material/select/md-select', nav: true, title: 'Select' },
-      { route: 'md-button',  name: 'md-button', moduleId: 'material/button/md-button', nav: true, title: 'Button' }
+      { route: 'md-button',  name: 'md-button', moduleId: 'material/button/md-button', nav: true, title: 'Button' },
+      { route: 'md-slider',  name: 'md-slider', moduleId: 'material/slider/md-slider', nav: true, title: 'Slider' }
     ]);
 
     this.router = router;
---
+++ src/material/slider/md-slider.js
@@ -0,0 +1,8 @@
+export class MdSliderDemo {
+  slides = [
+    { img: 'http://lorempixel.com/580/250/nature/1', align: 'center', heading: 'This is our big Tagline!', subheading: 'Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/2', align: 'left', heading: 'Left Aligned Caption', subheading: 'Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/3', align: 'right', heading: 'Right Aligned Caption', subheading: 'Here is our small slogan.' },
+    { img: 'http://lorempixel.com/580/250/nature/4', align: 'center', heading: 'This is another big Tagline!', subheading: 'Here is our small slogan.' }
+  ];
+}
