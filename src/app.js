f394edbf07d15d1fd123d8291c51251ef6948a1a
Daniel Bendel <daniel.bendel@gmail.com>
2016-02-03 21:19:05 +0100

    added select

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ export class App {
     config.map([
       { route: ['', 'welcome'], name: 'welcome',      moduleId: 'welcome',      nav: true, title: 'Welcome' },
       { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title: 'Github Users' },
-      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' }
+      { route: 'child-router',  name: 'child-router', moduleId: 'child-router', nav: true, title: 'Child Router' },
+      { route: 'md-select',  name: 'md-select', moduleId: 'material/select/md-select', nav: true, title: 'Select' }
     ]);
 
     this.router = router;
